29f58bde07a60f144ae5ddd5bef4935ea7a8f366
paolasilvayouse <paola.silva.rebase@youse.com.br>
2023-08-16 14:31:45 -0300

    Fix bug where quicksort was not considering an array with duplicate items

--- lib/recursion/quick_sort.rb
+++ lib/recursion/quick_sort.rb
@@ -3,8 +3,9 @@ module Recursion
     def self.qsort(list)
       return list if list.length < 2 
   
-      pivot = list.sample
-      list.delete(pivot)
+      random_pivot_index = (0..list.length - 1).to_a.sample
+      pivot = list[random_pivot_index]
+      list.delete_at(random_pivot_index)
       less = list.filter { |num| num <= pivot }
       greater = list.filter { |num| num > pivot }
   
--- spec/lib/recursion/quick_sort_spec.rb
+++ spec/lib/recursion/quick_sort_spec.rb
@@ -1,11 +1,11 @@
 describe Recursion::QuickSort do 
   context '.qsort' do 
     it 'Receives an unordered list and sorts it correctly' do 
-      list = [1, 0, 8, 4, 3]
+      list = [1, 1, 0, 8, 4, 3]
 
       result = Recursion::QuickSort.qsort(list)
 
-      expect(result).to eq [0, 1, 3, 4, 8]
+      expect(result).to eq [0, 1, 1, 3, 4, 8]
     end
   end
 end
